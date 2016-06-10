1067a6df3648a3c40747f28c061443c3c6b5ea15
Lee-YuMin <lym@hatiolabs-Mac-mini.local>
2016-06-10 16:07:15 +0900

    히든 수정

--- src/marker.js
+++ src/marker.js
@@ -14,33 +14,30 @@ export default class Marker extends Rect {
       height
     } = this.model;
 
-    if(!hidden){
+    context.translate(left, top)
 
-      context.translate(left, top)
+    context.beginPath()
+    context.lineJoin = "miter"
 
-      context.beginPath()
-      context.lineJoin = "miter"
+    context.moveTo(width / 2, height)
+    context.bezierCurveTo(width / 2, height * 3 / 4, 0, height / 2, 0, height / 4)
+    
+    context.ellipse(width / 2, height / 4, width / 2, height / 4, 0, Math.PI * 1, Math.PI * 0)
+    
+    context.bezierCurveTo(width, height / 2, width / 2, height * 3 / 4 , width / 2, height)
 
-      context.moveTo(width / 2, height)
-      context.bezierCurveTo(width / 2, height * 3 / 4, 0, height / 2, 0, height / 4)
-      
-      context.ellipse(width / 2, height / 4, width / 2, height / 4, 0, Math.PI * 1, Math.PI * 0)
-      
-      context.bezierCurveTo(width, height / 2, width / 2, height * 3 / 4 , width / 2, height)
+    // context.lineTo(width / 2, height)
 
-      // context.lineTo(width / 2, height)
+    // context.quadraticCurveTo(width / 2, height * 3 / 4, width / 2, height)
 
-      // context.quadraticCurveTo(width / 2, height * 3 / 4, width / 2, height)
+    // context.quadraticCurveTo(width / 2, height * 4 / 3, 0, height / 4)
 
-      // context.quadraticCurveTo(width / 2, height * 4 / 3, 0, height / 4)
+    context.closePath()
 
-      context.closePath()
-
-      this.drawFill(context)
-      this.drawStroke(context)
-      
-      context.translate(-left, -top)
-    }
+    this.drawFill(context)
+    this.drawStroke(context)
+    
+    context.translate(-left, -top)
   }
   get controls() {}
 }
