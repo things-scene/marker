500c1281f970ade34dcae11c207d98abefbe04a0
lllleeee <lcj1123@hatiolab.com>
2019-07-17 19:45:09 +0900

    fix 컴포넌트 반전 시 캔버스가 망가지는 현상
    close hatiolab/things-board#52

--- src/marker.js
+++ src/marker.js
@@ -8,24 +8,19 @@ const NATURE = {
   mutable: false,
   resizable: true,
   rotatable: true,
-  properties: [{
-    type: 'color',
-    label: 'inner-circle-fill-style',
-    name: 'innerCircleFillStyle',
-    property: 'innerCircleFillStyle'
-  }]
+  properties: [
+    {
+      type: 'color',
+      label: 'inner-circle-fill-style',
+      name: 'innerCircleFillStyle',
+      property: 'innerCircleFillStyle'
+    }
+  ]
 }
 
 export default class Marker extends Rect {
-
   _draw(context) {
-    var {
-      innerCircleFillStyle,
-      top,
-      left,
-      width,
-      height
-    } = this.model;
+    var { innerCircleFillStyle, top, left, width, height } = this.model
 
     context.translate(left, top)
 
@@ -33,11 +28,35 @@ export default class Marker extends Rect {
     context.beginPath()
 
     context.moveTo(width / 2, height * 0.9)
-    context.bezierCurveTo(width / 2.3, height * 0.6, 0, height / 2, 0, height / 4)
 
-    context.ellipse(width / 2, height / 4, width / 2, height / 4, 0, Math.PI * 1, Math.PI * 0)
+    context.bezierCurveTo(
+      width / 2.3,
+      height * 0.6,
+      0,
+      height / 2,
+      0,
+      height / 4
+    )
 
-    context.bezierCurveTo(width, height / 2, width / 1.7, height * 0.6, width / 2, height * 0.9)
+    context.ellipse(
+      width / 2,
+      height / 4,
+      Math.abs(width / 2),
+      Math.abs(height / 4),
+      0,
+      Math.PI * (width < 0 ? 0 : 1),
+      Math.PI * (width < 0 ? 1 : 0),
+      Math.sign(width) * Math.sign(height) == -1
+    )
+
+    context.bezierCurveTo(
+      width,
+      height / 2,
+      width / 1.7,
+      height * 0.6,
+      width / 2,
+      height * 0.9
+    )
 
     context.closePath()
 
@@ -46,7 +65,15 @@ export default class Marker extends Rect {
 
     // 안쪽 원 그리기
     context.beginPath()
-    context.ellipse(width / 2, height / 4, width * 0.18, height * 0.09, 0, 0, Math.PI * 2)
+    context.ellipse(
+      width / 2,
+      height / 4,
+      Math.abs(width * 0.18),
+      Math.abs(height * 0.09),
+      0,
+      0,
+      Math.PI * 2
+    )
     context.fillStyle = innerCircleFillStyle
     context.fill()
 
@@ -64,4 +91,3 @@ export default class Marker extends Rect {
 }
 
 Component.register('marker', Marker)
-
